e61133852e92515cc3ecea546152391c29541d2e
‌Behrouz Khezry <bkhezry@gmail.com>
2017-04-16 16:57:27 +0430

    add icon to bottom navigation.

--- app/src/main/java/com/github/bkhezry/demoextramapview/ui/MainActivity.java
+++ app/src/main/java/com/github/bkhezry/demoextramapview/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.github.bkhezry.demoextramapview;
+package com.github.bkhezry.demoextramapview.ui;
 
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.github.bkhezry.demoextramapview.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
