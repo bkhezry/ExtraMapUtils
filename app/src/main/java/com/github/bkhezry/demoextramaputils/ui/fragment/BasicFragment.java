6ca585ea0dfeeba7c47ff0852d0e37569e4f93f5
‌Behrouz Khezry <bkhezry@gmail.com>
2017-04-19 17:07:46 +0430

    add Area & Length ui to BasicFragment.

--- app/src/main/java/com/github/bkhezry/demoextramaputils/ui/fragment/BasicFragment.java
+++ app/src/main/java/com/github/bkhezry/demoextramaputils/ui/fragment/BasicFragment.java
@@ -6,22 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.bkhezry.demoextramaputils.R;
 import com.github.bkhezry.demoextramaputils.ui.activity.MapsActivity;
 import com.github.bkhezry.demoextramaputils.utils.AppUtils;
-import com.github.bkhezry.extramaputils.utils.MapUtils;
 import com.github.bkhezry.extramaputils.builder.ViewOptionBuilder;
+import com.github.bkhezry.extramaputils.model.ExtraPolygon;
+import com.github.bkhezry.extramaputils.model.ExtraPolyline;
 import com.github.bkhezry.extramaputils.model.ViewOption;
+import com.github.bkhezry.extramaputils.utils.MapUtils;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.Locale;
+
 
 public class BasicFragment extends Fragment implements OnMapReadyCallback {
     private MapView mMap;
+    private TextView areaTextView;
+    private TextView lengthTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,8 @@ public class BasicFragment extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_basic,
                 container, false);
+        areaTextView = (TextView) view.findViewById(R.id.area);
+        lengthTextView = (TextView) view.findViewById(R.id.length);
         mMap = (MapView) view.findViewById(R.id.mapLite);
         mMap.onCreate(savedInstanceState);
         mMap.getMapAsync(this);
@@ -48,6 +57,7 @@ public class BasicFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getActivity());
         final ViewOption viewOption = getViewOption();
+        calculateAreaAndLength(viewOption);
         MapUtils.showElements(viewOption, googleMap, getActivity());
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
@@ -59,6 +69,19 @@ public class BasicFragment extends Fragment implements OnMapReadyCallback {
                 startActivity(intent);
             }
         });
+    }
+
+    private void calculateAreaAndLength(ViewOption viewOption) {
+        double area = 0.0;
+        double length = 0.0;
+        for (ExtraPolygon extraPolygon : viewOption.getPolygons()) {
+            area += extraPolygon.getArea();
+        }
+        areaTextView.setText(String.format(Locale.ENGLISH, "%.2f", area) + getString(R.string.square_meters));
+        for (ExtraPolyline extraPolyline : viewOption.getPolylines()) {
+            length += extraPolyline.getLength();
+        }
+        lengthTextView.setText(String.format(Locale.ENGLISH, "%.2f", length) + getString(R.string.meters));
     }
 
     public static ViewOption getViewOption() {
