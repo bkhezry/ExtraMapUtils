5e985cca74d84136ef07b62e134b0d973aa92963
Behrouz Khezry <bkhezry@gmail.com>
2017-04-24 21:49:39 +0430

    replace doTransaction function with FragmentManager library.

--- app/src/main/java/com/github/bkhezry/demoextramaputils/ui/activity/MainActivity.java
+++ app/src/main/java/com/github/bkhezry/demoextramaputils/ui/activity/MainActivity.java
@@ -4,67 +4,54 @@ package com.github.bkhezry.demoextramaputils.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.aspsine.fragmentnavigator.FragmentNavigator;
 import com.github.bkhezry.demoextramaputils.R;
-import com.github.bkhezry.demoextramaputils.ui.fragment.AboutFragment;
-import com.github.bkhezry.demoextramaputils.ui.fragment.BasicFragment;
-import com.github.bkhezry.demoextramaputils.ui.fragment.DataLoaderFragment;
-import com.github.bkhezry.demoextramaputils.ui.fragment.ListViewFragment;
+import com.github.bkhezry.demoextramaputils.adapter.FragmentAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private Fragment mFragment = new BasicFragment().newInstance();
-    private FragmentManager mFragmentManager;
     private BottomNavigationView navigation;
     private String title = "Basic";
-
+    private FragmentNavigator fragmentNavigator;
     public BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_basic:
-                    mFragment = new BasicFragment().newInstance();
+                    fragmentNavigator.showFragment(0);
                     title = getResources().getString(R.string.title_basic);
                     break;
                 case R.id.navigation_loader:
-                    mFragment = new DataLoaderFragment().newInstance();
+                    fragmentNavigator.showFragment(1);
                     title = getResources().getString(R.string.title_loader);
                     break;
                 case R.id.navigation_list:
-                    mFragment = new ListViewFragment().newInstance();
+                    fragmentNavigator.showFragment(2);
                     title = getResources().getString(R.string.title_listview);
                     break;
                 case R.id.navigation_about:
-                    mFragment = new AboutFragment().newInstance();
+                    fragmentNavigator.showFragment(3);
                     title = getResources().getString(R.string.title_about);
                     break;
             }
-            doTransaction();
+            setTitle(title);
             return true;
         }
 
     };
 
-    private void doTransaction() {
-        if (mFragment != null) {
-            setTitle(title);
-            mFragmentManager.beginTransaction()
-                    .replace(R.id.contentFrameLayout, mFragment).commit();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        mFragmentManager = getSupportFragmentManager();
+        fragmentNavigator = new FragmentNavigator(getSupportFragmentManager(), new FragmentAdapter(), R.id.contentFrameLayout);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        doTransaction();
+        fragmentNavigator.showFragment(0);
     }
 
 }
